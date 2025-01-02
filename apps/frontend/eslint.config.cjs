7863dab7a777cf994531edf68d06227469760605
Felix <jonfelixrico@gmail.com>
2025-01-02 19:10:30 +0800

    build: nx's eslint can suck my dick

--- apps/frontend/eslint.config.cjs
+++ apps/frontend/eslint.config.cjs
@@ -1,32 +1,9 @@
-const { FlatCompat } = require('@eslint/eslintrc')
-const js = require('@eslint/js')
-const baseConfig = require('../../eslint.config.cjs')
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-})
-
-module.exports = [
-  ...baseConfig,
-  ...compat.extends('@nuxt/eslint-config'),
-  {
-    files: ['**/*.vue'],
-    languageOptions: {
-      parserOptions: {
-        parser: require('@typescript-eslint/parser'),
-      },
-    },
+module.exports = {
+  root: true,
+  extends: ['@nuxt/eslint-config', 'prettier'],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
   },
-  {
-    ignores: ['.nuxt/**', '.output/**', 'node_modules'],
-  },
-  {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx', '**/*.vue'],
-    // Override or add rules here
-    rules: {
-      'vue/max-attributes-per-line': 'off',
-      'vue/singleline-html-element-content-newline': 'off',
-    },
-  },
-]
+}
